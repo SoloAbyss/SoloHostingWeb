1ca1299cc870e7876e0edb8175fe04afc3cd132e
Maximus <maximusabrahamse@gmail.com>
2023-02-22 21:37:13 +1300

    Hosting Website

---
+++ script.js
@@ -0,0 +1,11 @@
+const darkModeButton = document.getElementById('dark-mode-btn');
+const body = document.querySelector('body');
+
+darkModeButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
+
+var darkModeBtn = document.getElementById('dark-mode-btn');
+
+
+
