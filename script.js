9739427ab8598c5774982b6dc0ad6183f7584717
Maximus <maximusabrahamse@gmail.com>
2023-02-23 18:36:29 +1300

    Made light mode sync between reloads/pages

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const darkModeButton = document.getElementById('dark-mode-btn');
-const body = document.querySelector('body');
+const lightModeButton = document.getElementById("light-mode-btn");
+const body = document.querySelector("body");
 
-darkModeButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
+let settings = JSON.parse(window.localStorage.getItem("Settings") ?? "{}");
+if (settings.light_mode) {
+    body.classList.add('light-mode');
+}
+
+lightModeButton.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+  settings.light_mode = body.classList.contains("light-mode");
 });
 
-var darkModeBtn = document.getElementById('dark-mode-btn');
-
-
-
+window.addEventListener("unload", () => {
+  window.localStorage.setItem("Settings", JSON.stringify(settings));
+});
