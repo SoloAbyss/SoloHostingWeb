c6adf8efc7a99b98e4f8d49a041d243518686e69
Maximus <maximusabrahamse@gmail.com>
2023-02-23 18:59:56 +1300

    changed light mode button to say dark mode when in light mode and vice verca

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const lightModeButton = document.getElementById("light-mode-btn");
 const body = document.querySelector("body");
 
 let settings = JSON.parse(window.localStorage.getItem("Settings") ?? "{}");
-if (settings.light_mode) {
-    body.classList.add('light-mode');
-}
+if (settings.light_mode) { 
+  body.classList.add("light-mode");
+  lightModeButton.innerText = "Dark Mode";
+} else {
+  lightModeButton.innerText = "Light Mode";
+}  
 
 lightModeButton.addEventListener("click", () => {
   body.classList.toggle("light-mode");
   settings.light_mode = body.classList.contains("light-mode");
+  if (settings.light_mode) {
+    lightModeButton.innerText = "Dark Mode";
+  } else {
+    lightModeButton.innerText = "Light Mode";
+  }
 });
 
 window.addEventListener("unload", () => {
